e22d044c31856c3b804e3e24f2621cbd22a5217a
Sowjanya_Kavuri <97374557+Sowjichows@users.noreply.github.com>
2026-04-06 03:54:36 +0000

    Lab submission

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const path = require('path');
+const EventEmitter = require('events');
+
+const port = process.env.PORT || 3000;
+const chatEmitter = new EventEmitter();
+
+/**
+ * Responds with plain text
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+/**
+ * Responds with JSON
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondJson(req, res) {
+  // express has a built in json method that will set the content type header
+  res.json({
+    text: 'hi',
+    numbers: [1, 2, 3],
+  });
+}
+/**
+ * Responds with a 404 not found
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondNotFound(req, res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+}
+/**
+ * Responds with the input string in various formats
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondEcho (req, res) {
+  // req.query is an object that contains the query parameters
+  const { input = '' } = req.query;
+
+  // here we make use of res.json to send a json response with less code
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    charCount: input.length,
+    backwards: input.split('').reverse().join(''),
+  });
+}
+// app.js
+
+function chatApp(req, res) {
+  res.sendFile(path.join(__dirname, '/chat.html'));
+}
+
+
+// register the endpoint with the app (make sure to remove the old binding to the `/` route)
+const app = express();
+app.get('/', chatApp);
+// function declarations for respondText, respondJson, respondNotFound and respondEcho stay here
+app.get('/', respondText);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+
+app.get('/chat', respondChat);
+
+function respondChat (req, res) {
+  const { message } = req.query;
+
+  chatEmitter.emit('message', message);
+  res.end();
+}
+/**
+ * This endpoint will respond to the client with a stream of server sent events
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+app.get('/sse', respondSSE);
+
+function respondSSE (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+  });
+
+  const onMessage = message => res.write(`data: ${message}\n\n`); // use res.write to keep the connection open, so the client is listening for new messages
+  chatEmitter.on('message', onMessage);
+
+  res.on('close', () => {
+    chatEmitter.off('message', onMessage);
+  });
+}
+// add this line just after we declare the express app
+app.use(express.static(__dirname + '/public'));
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
